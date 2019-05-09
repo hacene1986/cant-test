fb4c6bc414f0586b3a73a6d63df862d3a2b065a1
hacene1986 <hacene.kherbouche@gmail.com>
2019-05-09 13:20:44 +0200

    modif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlatCantinereComponent } from './plat-cantinere/plat-cantinere.component';
+import { MenuCantinereComponent } from './menu-cantinere/menu-cantinere.component';
+import { RecapCantiniereComponent } from './recap-cantiniere/recap-cantiniere.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {'path': 'platCantiniere', 'component': PlatCantinereComponent},
+  {'path': 'menuCantiniere', 'component': MenuCantinereComponent},
+  {'path': 'recapCantiniere', 'component': RecapCantiniereComponent},
+  {'path': 'contact', 'component': ContactComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,33 +8,21 @@ import { AppComponent } from './app.component';
 import { IsncriptionComponent } from './auth/isncription/isncription.component';
 import { ConnexionComponent } from './auth/connexion/connexion.component';
 import { ContactComponent } from './contact/contact.component';
-import { MenuComponent } from './menu/menu.component';
+
 import { HeaderComponent } from './header/header.component';
-import { PlatComponent } from './plat/plat.component';
+
 import { PlatCantinereComponent } from './plat-cantinere/plat-cantinere.component';
 import { MenuCantinereComponent } from './menu-cantinere/menu-cantinere.component';
 import { RecapCantiniereComponent } from './recap-cantiniere/recap-cantiniere.component';
 
-const appRoute: Routes = [
-  {path: 'auth/inscription', component: IsncriptionComponent},
-  {path: 'auth/connexion', component: ConnexionComponent},
-  {path: 'menu', component: MenuComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'plat', component: PlatComponent},
-  {path: 'platCantiniere', component: PlatCantinereComponent},
-  {path: 'menuCantiniere', component: MenuCantinereComponent},
-  {path: 'recapCantiniere', component: RecapCantiniereComponent}
- 
-]
+
 @NgModule({
   declarations: [
     AppComponent,
     IsncriptionComponent,
     ConnexionComponent,
     ContactComponent,
-    MenuComponent,
     HeaderComponent,
-    PlatComponent,
     PlatCantinereComponent,
     MenuCantinereComponent,
     RecapCantiniereComponent
@@ -42,7 +30,6 @@ const appRoute: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoute),
     HttpClientModule 
 
   ],
